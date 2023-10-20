8c18813bca6bfc72de5b6a00e926259aba63bbfb
Nick <76213241+rorry47@users.noreply.github.com>
2023-10-20 20:52:50 +0300

    Create config.php

---
+++ modules/servers/mailcow/config.php
@@ -0,0 +1,22 @@
+<?php
+// SETTINGS
+// limit count of aliases associated with this domain
+$SET_aliases = 100; 
+
+// limit count of mailboxes associated with this domain
+$SET_MMAILBOXES = 10; 
+
+// maximum quota per mailbox
+$SET_MAILBOXQUOTA = 1024;
+
+// predefined mailbox quota in add mailbox form
+$SET_DEFQUOTA = 1024;
+
+// maximum quota for this domain (for all mailboxes in sum)
+$SET_MAILBOXES = 10240;
+
+// rate limit value
+$SET_rl_value = 10;
+
+// s - per second, m - per minute, h - per hour, d - per day
+$SET_rl_frame = "s";
